e593395ea572c9851ec67d874edfdca9f8932eb9
Abigail Limpioso <alimpioso@gmail.com>
2020-08-06 03:44:54 -0400

    added initial files and component

---
+++ employee-directory/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Directory from './components/directory';
+
+function App() {
+  return (
+    <div className="container">
+        <h1>Employee Directory</h1>
+         <Directory/>
+    </div>
+  );
+}
+
+export default App;
---
+++ employee-directory/src/components/directory.js
@@ -0,0 +1,22 @@
+import React, {Component} from "react";
+
+class Directory extends Component {
+
+    render() {
+        return (
+            <table>
+                <tbody>
+
+                    <tr>
+                        <td>Name</td>
+                        <td>Title</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        )
+    }
+
+}
+
+export default Directory;
