b110c374f70de122fc94f1bce7ca8b1160a3a7f7
Abigail Limpioso <alimpioso@gmail.com>
2020-08-08 21:17:51 -0400

    added css and data

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,12 +1,96 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import './App.css';
 import Directory from './components/directory';
 
 function App() {
+
+  const employees = [
+    {
+      name: "Jake Gyllenhal",
+      title: "Software Engineer",
+      phone: "952-452-2390",
+      email: "jake@designzillas.com",
+      department: "IT"
+    },
+    {
+      name: "Edward Cullen",
+      title: "HR Manager",
+      phone: "349-433-2320",
+      email: "edward@designzillas.com",
+      department: "Human Resources"
+    },
+    {
+      name: "Bella Thorne",
+      title: "HR Representative",
+      phone: "934-390-8492",
+      email: "bella@designzillas.com",
+      department: "Human Resources"
+    },
+    {
+      name: "Allison Hargreeves",
+      title: "Test Engineer",
+      phone: "954-204-9140",
+      email: "allison@designzillas.com",
+      department: "Development"
+    },
+    {
+      name: "Harry Potter",
+      title:"Back-End Developer",
+      phone: "939-290-4782",
+      email: "harry@designzillas.com",
+      department: "Development"
+    },
+    {
+      name: "Hermione Granger",
+      title: "Back-End Developer",
+      phone: "934-452-2591",
+      email: "hermione@designzillas.com",
+      department: "Development"
+    },
+    {
+      name: "Hayley Williams",
+      title: "Office Puppy",
+      phone: "952-452-2390",
+      email: "supportpup@designzillas.com",
+      department: "Human Resources"
+    },
+    {
+      name: "Taylor Swift",
+      title: "Web Designer",
+      phone: "324-390-1049",
+      email: "taylor@designzillas.com",
+      department: "Design"
+    },
+    {
+      name: "Lisa Simpson",
+      title: "UX Designer",
+      phone: "342-134-5032",
+      email: "lisa@designzillas.com",
+      department: "Design"
+    },
+    {
+      name: "Thomas Black",
+      title: "Software Engineer",
+      phone: "324-547-2492",
+      email: "tom@designzillas.com",
+      department: "IT"
+    }
+  ]
+
+  const [allEmployees, dispatch] = useReducer((state, action) => {
+
+    if(action === "sortByName") {
+
+      return state;
+
+    }
+
+  }, employees);
+
   return (
     <div className="container">
         <h1>Employee Directory</h1>
-         <Directory/>
+         <Directory allEmployees={allEmployees} onClick={() => dispatch("sortByName")}/>
     </div>
   );
 }
--- employee-directory/src/components/directory.js
+++ employee-directory/src/components/directory.js
@@ -1,24 +1,36 @@
-import React, {Component} from "react";
+import React from "react";
 
-class Directory extends Component {
+const Directory = ({onClick, allEmployees}) => {
 
-    render() {
+        const listedEmployees = allEmployees.map((employee) => 
+
+            <tr>
+                <td>{employee.name}</td>
+                <td>{employee.title}</td>
+                <td>{employee.department}</td>
+                <td>{employee.phone}</td>
+                <td>{employee.email}</td>
+            </tr>
+        
+        );
+  
         return (
             <table>
                 <tbody>
 
                     <tr>
-                        <td>Name</td>
-                        <td>Job Title</td>
-                        <td>Department</td>
-                        <td>Phone Number</td>
-                        <td>Email Address</td>
+                        <th>Name<span class="sort-by-name" onClick={onClick}><i class="fas fa-angle-down"></i></span></th>
+                        <th>Job Title</th>
+                        <th>Department</th>
+                        <th>Phone Number</th>
+                        <th>Email Address</th>
                     </tr>
+
+                    {listedEmployees}
 
                 </tbody>
             </table>
         )
-    }
 
 }
 
