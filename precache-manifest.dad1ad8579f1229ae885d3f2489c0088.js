af91961341724d7f7c14aff36d828982b5a8f4cf
Abigail Limpioso <alimpioso@gmail.com>
2020-08-08 22:37:58 -0400

    Updates

---
+++ precache-manifest.dad1ad8579f1229ae885d3f2489c0088.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98f5057753fab7bbe20d9018563f6ef1",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "c61e532d5384cb20d277",
+    "url": "/Employee-Directory/static/css/main.739d1a7c.chunk.css"
+  },
+  {
+    "revision": "c97e1e40b523aaa72304",
+    "url": "/Employee-Directory/static/js/2.ab4fd589.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.ab4fd589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c61e532d5384cb20d277",
+    "url": "/Employee-Directory/static/js/main.432741bb.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
